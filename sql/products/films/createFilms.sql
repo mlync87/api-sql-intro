5f5684ff37e5a0ecf8b29e301f20c776c21393fe
mlync87 <martylynch35@hotmail.com>
2023-01-09 17:12:05 +0000

    core completed

---
+++ sql/products/films/createFilms.sql
@@ -0,0 +1,7 @@
+CREATE TABLE films (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR (50),
+    genre VARCHAR (50),
+    year_released int,
+    score int,
+);
---
+++ sql/products/films/selectFilms.sql
@@ -0,0 +1,27 @@
+SELECT * FROM films
+
+SELECT * FROM films ORDER BY score DESC
+
+SELECT * FROM films ORDER BY release_year ASC
+
+SELECT * FROM films WHERE score >= 8
+
+SELECT * FROM films WHERE score <= 7
+
+SELECT * FROM films WHERE release_year = 1990
+
+SELECT * FROM films WHERE release_year < 2000
+
+SELECT * FROM films WHERE release_year > 1990
+
+SELECT * FROM films WHERE release_year BETWEEN 1990 AND 1999
+
+SELECT * FROM films WHERE genre = 'SciFi'
+
+SELECT * FROM films WHERE genre = 'SciFi' OR genre = 'Western'
+
+SELECT * FROM films WHERE genre != 'SciFi'
+
+SELECT * FROM films WHERE genre = 'Western' AND release_year < 2000
+
+SELECT * FROM films WHERE title LIKE 'The Matrix%' 
